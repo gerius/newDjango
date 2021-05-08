9385da339a741e360dc811aaa70b5dfaff8de003
gerius <geriusfouyet@gmail.com>
2021-05-08 13:09:25 +0100

    new update

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sampledb',
+        'USER': 'user0BT',
+        'PASSWORD': 'fCrYC2gj5awuiHw1',
+        'HOST': 'postgresql',
+        'PORT': '5432',
     }
 }
 
